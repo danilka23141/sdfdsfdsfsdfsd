4038136a2f2c2d64db972d21e1a4e584e08bd951
Mikhalev Daniil <mixalyov1994@list.ru>
2024-07-16 13:40:31 +0300

    Редакт11

--- src/main/java/StatsService.java
+++ src/main/java/StatsService.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class StatsService {
+
+
+public class StatsService {
+
+    public long sum(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public long average(long[] sales) {
+        return sum(sales) / sales.length;
+    }
+
+    public int maxSalesMonth(long[] sales) {
+        int maxMonth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSalesMonth(long[] sales) {
+        int minMonth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth + 1;
+    }
+
+    public int belowAverageMonths(long[] sales) {
+        long average = average(sales);
+        int belowAverageCount = 0;
+        for (long sale : sales) {
+            if (sale < average) {
+                belowAverageCount++;
+            }
+        }
+        return belowAverageCount;
+    }
+
+    public int aboveAverageMonths(long[] sales) {
+        long average = average(sales);
+        int aboveAverageCount = 0;
+        for (long sale : sales) {
+            if (sale > average) {
+                aboveAverageCount++;
+            }
+        }
+        return aboveAverageCount;
+    }
 }
--- src/test/java/StatsServiceTest.java
+++ src/test/java/StatsServiceTest.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class StatsServiceTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+    @Test
+    void shouldCalculateSum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 192;
+        long actual = service.sum(sales);
+
+
+    }
+
+    @Test
+    void shouldCalculateAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 16;
+        long actual = service.average(sales);
+
+
+    }
+
+    @Test
+    void shouldFindMaxSalesMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 7;
+        int actual = service.maxSalesMonth(sales);
+
+
+    }
+
+    @Test
+    void shouldFindMinSalesMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.minSalesMonth(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCountBelowAverageMonths() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.belowAverageMonths(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCountAboveAverageMonths() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.aboveAverageMonths(sales);
+
+        assertEquals(expected, actual);
+    }
 }
